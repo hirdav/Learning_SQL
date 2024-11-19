d394111f37f193d2ba37c3a8ff761766ec46dd60
Hirak Jyoti Kalita <hirakjkalita71@gmail.com>
2024-11-19 21:25:19 +0530

    create a table and store data to and viewed the data

---
+++ 2_TIY.sql
@@ -0,0 +1,14 @@
+ --Here are two exercises to help you explore concepts related to databases, tables, and
+ --data relationships:
+ --Imagine you’re building a database to catalog all the animals at your local zoo. You
+ --want one table to track the kinds of animals in the collection and another table to track
+ --the specifics on each animal. Write CREATE TABLE statements for each table that include
+ --some of the columns you need. Why did you include the columns you chose?
+ --Now create INSERT statements to load sample data into the tables. How can you view
+ --the data via the pgAdmin tool? Create an additional INSERT statement for one of your
+ --tables. Purposely omit one of the required commas separating the entries in the VALUES
+ --clause of the query. What is the error message? Would it help you find the error in the
+ --code?
+
+
+SELECT * FROM  public.zoo
